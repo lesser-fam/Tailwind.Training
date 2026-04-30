0c76f71b59c0153ea5af5e3dacf0676084ca6a5f
Hayate Teruuchi <hayate.t.work@gmail.com>
2026-04-30 10:16:38 +0900

    TailwindのPrettierプラグイン、拡張機能インストール

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-                mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
-            },
-            colors: {
-                "custom-blue": "#1E40AF",
-                "custom-green": {
-                    light: "#6EE7B7",
-                    DEFAULT: "#10B981",
-                    dark: "#047857",
-                    "custom-purple": "#8f0e91",
-                },
-            },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: "#F05340", // Laravel Red
+          secondary: "#6c757d", // Gray
         },
+      },
     },
-    plugins: [
-        plugin(function ({ addUtilities }) {
-            addUtilities({
-                ".writing-vertical-rl": {
-                    "writing-mode": "vertical-rl",
-                },
-            });
-        }),
-    ],
+  },
+  plugins: [],
 };
