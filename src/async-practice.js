e1cbf2a13ece584e6651c54bcc79c27f9d2d8f88
Hayate Teruuchi <hayate.t.work@gmail.com>
2026-05-16 16:19:14 +0900

    非同期処理:ハンズオン 2

--- src/async-practice.js
+++ src/async-practice.js
@@ -1,14 +1,33 @@
 // 実行順を予測
-console.log("A");
+// console.log("A");
 
-setTimeout(() => {
-    console.log("B");
-}, 0);
+// setTimeout(() => {
+//     console.log("B");
+// }, 0);
 
-console.log("C");
+// console.log("C");
 
-setTimeout(() => {
-    console.log("D");
-}, 1000);
+// setTimeout(() => {
+//     console.log("D");
+// }, 1000);
 
-console.log("E");
+// console.log("E");
+
+// カウントダウンタイマー
+function countdown(seconds) {
+    console.log(`カウントダウン開始：${seconds}秒`);
+
+    for (let i = seconds; 0 <= i; i--) {
+        setTimeout(
+            () => {
+                if (i > 0) {
+                    console.log(i);
+                } else {
+                    console.log("完了！");
+                }
+            },
+            (seconds - i) * 1000,
+        );
+    }
+}
+countdown(5);
