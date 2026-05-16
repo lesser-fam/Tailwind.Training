f9a7116fc9704872784b66bec9efd5fef562b085
Hayate Teruuchi <hayate.t.work@gmail.com>
2026-05-16 16:41:59 +0900

    非同期処理:ハンズオン 3

--- src/async-practice.js
+++ src/async-practice.js
@@ -14,20 +14,55 @@
 // console.log("E");
 
 // カウントダウンタイマー
-function countdown(seconds) {
-    console.log(`カウントダウン開始：${seconds}秒`);
+// function countdown(seconds) {
+//     console.log(`カウントダウン開始：${seconds}秒`);
 
-    for (let i = seconds; 0 <= i; i--) {
-        setTimeout(
-            () => {
-                if (i > 0) {
-                    console.log(i);
-                } else {
-                    console.log("完了！");
-                }
-            },
-            (seconds - i) * 1000,
-        );
-    }
+//     for (let i = seconds; 0 <= i; i--) {
+//         setTimeout(
+//             () => {
+//                 if (i > 0) {
+//                     console.log(i);
+//                 } else {
+//                     console.log("完了！");
+//                 }
+//             },
+//             (seconds - i) * 1000,
+//         );
+//     }
+// }
+// countdown(5);
+
+// データ取得シミュレーション(未習熟)
+function fetchData(id) {
+    return new Promise((resolve, reject) => {
+        console.log(`データ取得中...（ID：${id}`);
+
+        setTimeout(() => {
+            if (id <= 0) {
+                reject(new Error("無効なIDです"));
+            } else {
+                resolve({
+                    id: id,
+                    title: `データ ${id}`,
+                    createdAt: new Data().toISOString(),
+                });
+            }
+        }, 1000);
+    });
 }
-countdown(5);
+
+fetchData(1)
+    .then((data) => {
+        console.log("取得成功:", data);
+    })
+    .catch((error) => {
+        console.error("エラー:", error.message);
+    });
+
+fetchData(-1)
+    .then((data) => {
+        console.log("取得成功:", data);
+    })
+    .catch((error) => {
+        console.error("エラー:", error.message);
+    });
